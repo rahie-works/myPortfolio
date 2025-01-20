30506ae43323b67f7d111c309127d276e4d68913
rahie-works <raheesh.work@gmail.com>
2025-01-19 21:53:52 -0500

    update

--- my-portfolio/src/app/HomePage/HomePageProfileInfoContainer.tsx
+++ my-portfolio/src/app/HomePage/HomePageProfileInfoContainer.tsx
@@ -7,6 +7,29 @@ import {
 } from "react-icons/fa";
 
 export const HomePageProfileInfoContainer = () => {
+  const handleSocialClick = (link: string) => {
+    switch (link) {
+      case "LinkedIn":
+        window.open(
+          "https://www.linkedin.com/in/raheesh-k-muhamed-8b3580129/",
+          "_blank"
+        );
+        break;
+      case "Github":
+        window.open("https://github.com/rahie-works", "_blank");
+        break;
+      case "Instagram":
+        window.open("https://www.instagram.com/raheesh_k_muhamed/", "_blank");
+        break;
+      case "StackOverflow":
+        window.open(
+          "https://stackoverflow.com/users/17244811/raheesh-kattumunda-muhamed",
+          "_blank"
+        );
+        break;
+    }
+  };
+
   return (
     <div className="w-full h-full pt-32 pb-10 md:pt-0 md:pb-0 md:w-1/4 md:h-full flex flex-col bg-gradient-to-b from-transparent to-gray-900 shadow-lg rounded-lg animate-fade-up animate-ease-in justify-center items-center">
       <Image
@@ -25,10 +48,13 @@ export const HomePageProfileInfoContainer = () => {
           <p className="text-sm">Ontario, CA</p>
         </div>
         <div className="w-full flex justify-center gap-16 md:gap-5">
-          <FaLinkedin className="text-blue-700 text-3xl md:text-2xl" />
-          <FaGithub className="text-white text-3xl md:text-2xl" />
-          <FaInstagram className="text-pink-700 text-3xl md:text-2xl" />
-          <FaStackOverflow className="text-red-500 text-3xl md:text-2xl" />
+          <FaLinkedin
+            className="text-blue-700 text-3xl md:text-4xl"
+            onClick={() => handleSocialClick("LinkedIn")}
+          />
+          <FaGithub className="text-white text-3xl md:text-4xl" />
+          <FaInstagram className="text-pink-700 text-3xl md:text-4xl" />
+          <FaStackOverflow className="text-red-500 text-3xl md:text-4xl" />
         </div>
         <button className="border-2 border-blue-500 text-white text-lg md:text-xs p-3 rounded-lg cursor-pointer">
           Lets Connect
